42679aed2477e967e50816701ca7073f867c626e
Joshua Alvarez <joshuayherrera@gmail.com>
2024-07-21 22:04:44 -0500

    Pricing Section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,6 +2,7 @@ import { Hero } from "@/sections/Hero";
 import { Header } from "@/sections/Header";
 import { LogoTicker } from "@/sections/LogoTicker";
 import { ProductShowcase } from "@/sections/ProductShowcase";
+import { Pricing } from "@/sections/Pricing";
 
 export default function Home() {
   return (
@@ -10,6 +11,7 @@ export default function Home() {
       <Hero />
       <LogoTicker />
       <ProductShowcase />
+      <Pricing />
     </>
 );
 }
